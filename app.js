fd1d4fb1216119ae151f1d55eaa8859be14d187a
Raghava P <patnam.raghava@gmail.com>
2016-09-13 23:15:33 +0530

    Adding changes in the app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 (function(){
     console.log("First App in the git application");
     // Adding a new comment
+
+    var alertFun = function(){
+        console.alert("alert Function is called")
+    }
+    alertFun();
 })();
